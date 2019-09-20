a0302c89f0c5831678650549069ce8b4770d85af
CivicToyota <38553164+CivicToyota@users.noreply.github.com>
2019-09-19 22:31:27 -0700

    textbox

---
+++ client/my-app/components/textbox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Form, TextArea } from 'semantic-ui-react'
+
+const TextAreaExampleTextArea = () => (
+  <Form>
+    <TextArea placeholder='Write an announcement' />
+  </Form>
+)
+
+export default TextAreaExampleTextArea
